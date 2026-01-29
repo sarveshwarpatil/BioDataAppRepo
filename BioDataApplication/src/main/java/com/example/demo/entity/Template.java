a7d3af8ff8f5f6502512524b2db7c39a80a77284
sarveshwarpatil <134357325+sarveshwarpatil@users.noreply.github.com>
2026-01-29 12:27:11 +0530

    Update Template.java
    merge

--- BioDataApplication/src/main/java/com/example/demo/entity/Template.java
+++ BioDataApplication/src/main/java/com/example/demo/entity/Template.java
@@ -5,6 +5,7 @@ public class Template {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("remote change");
+		System.out.println("remote merge");
 	}
 
 }
